965a674b4b82c41909cd24a512c286213b7675b2
persianProgrammers <108981932+persianProgrammers@users.noreply.github.com>
2024-07-22 18:42:28 +0330

    Everithing from Session 1 to 34

---
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteLagoon.Domain.Entities
+{
+    public class Villa
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public required int Price { get; set; }
+        public required int Sqft { get; set; }
+        public required int Occupancy { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+}
---
+++ WhiteLagoon.InfruStructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Infrustructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
